e4d70c83ab53eddf64c0d9bb22658da4df2ce9cd
Daniel Bujarski <daniel.tov@hotmail.com>
2024-03-17 20:31:13 -0700

    Adding images

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Smooth scrolling
+    const links = document.querySelectorAll('a[href^="#"]');
+    links.forEach(link => {
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+
+});
+
+
+const scrollToTopButton = document.getElementById('scrollToTopButton');
+
+scrollToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
+
